f2e3480b97b83112f5f1c87ea1ff2bc85779185f
Alex <108487553+Al3xramirez@users.noreply.github.com>
2026-02-19 21:38:49 -0800

    fixed package structure

--- backend-springboot/src/main/java/com/cscd488seniorproject/syllabussyncproject/HealthController.java
+++ backend-springboot/src/main/java/com/cscd488seniorproject/syllabussyncproject/HealthController.java
@@ -1,4 +1,4 @@
-package com.cscd488SeniorProject.SyllabusSync;
+package com.cscd488seniorproject.syllabussyncproject;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
