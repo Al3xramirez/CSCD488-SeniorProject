9ba84cf2fe035f3185ba90ec19bd1c0d1fd3ec4f
Ben <benjaminburns1021@gmail.com>
2026-02-21 18:56:41 -0800

    removing some redundancy

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-=======
-<script setup>
-import { ref, onMounted } from 'vue'
-
-const msg = ref('Loading...')
-
-onMounted(async () => {
-  const res = await fetch('/api/health')
-  msg.value = await res.text()
-})
-
-</script>
-
->>>>>>> testing/LocalDBSetup
 <template>
   <div>
     <router-view></router-view>
--- frontend-vue/src/views/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<main>
-    <h1>
-        Home
-    </h1>
-    <button>logout</button>
-</main>
-
-</template>
