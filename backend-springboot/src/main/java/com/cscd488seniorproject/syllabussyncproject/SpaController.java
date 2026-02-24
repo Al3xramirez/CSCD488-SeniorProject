aa80c08a84db05cdc8750336485881c2c70d3c59
Alex <108487553+Al3xramirez@users.noreply.github.com>
2026-02-23 19:53:54 -0800

    more adaptable controller

--- backend-springboot/src/main/java/com/cscd488seniorproject/syllabussyncproject/SpaController.java
+++ backend-springboot/src/main/java/com/cscd488seniorproject/syllabussyncproject/SpaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SpaController {
 
     // This fowards all non "/api" requests to index.html so that the vue router can handle the routing on the frontend
-    @RequestMapping(value = {"/{path:^(?!api).*$}", "/**/{path:^(?!api).*$}"})
+    @RequestMapping(value = { "/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
     public String foward() {
         return "forward:/index.html";
     }
