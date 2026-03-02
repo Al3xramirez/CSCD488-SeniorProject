4e26c3b582b68a3c65f8f786e58e274b81f6cae4
Alex <108487553+Al3xramirez@users.noreply.github.com>
2026-03-01 18:50:58 -0800

    bugfix for login rerouting to springboots default login instead of ours

--- backend-springboot/src/main/java/com/cscd488seniorproject/syllabussyncproject/SpaController.java
+++ backend-springboot/src/main/java/com/cscd488seniorproject/syllabussyncproject/SpaController.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpaController {
 
-    // This fowards all non "/api" requests to index.html so that the vue router can handle the routing on the frontend
+    //This fowards all non "/api" requests to index.html so that the vue router can handle the routing on the frontend
     @RequestMapping(value = { "/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
     public String foward() {
         return "forward:/index.html";
-    }
+   }
+   
 }
