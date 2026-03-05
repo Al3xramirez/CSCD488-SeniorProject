4c8cdf7812986313a82ba2ac3693751b4cfccbc2
raulreano-juarez <rreanojuarez@ewu.edu>
2026-03-04 17:49:54 -0800

    Ical Structure upload

---
+++ backend-springboot/src/main/java/com/cscd488seniorproject/syllabussyncproject/Service/CalendarSync.java
@@ -0,0 +1,5 @@
+package com.cscd488seniorproject.syllabussyncproject.Service;
+
+public class CalendarSync {
+    
+}
---
+++ backend-springboot/src/main/java/com/cscd488seniorproject/syllabussyncproject/controllers/CalendarController.java
@@ -0,0 +1,5 @@
+package com.cscd488seniorproject.syllabussyncproject.controllers;
+
+public class CalendarController {
+    
+}
