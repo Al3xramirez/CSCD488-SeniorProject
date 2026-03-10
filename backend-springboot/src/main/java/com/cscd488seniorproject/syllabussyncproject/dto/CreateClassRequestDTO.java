8cf1747c096f6b1c7c287edabe8171b9f38b4e68
Alex <108487553+Al3xramirez@users.noreply.github.com>
2026-03-10 14:25:28 -0700

    DTO's for class creation and summary
    Added DTO's that we'll probably need for student and professor summary's of their classes.

---
+++ backend-springboot/src/main/java/com/cscd488seniorproject/syllabussyncproject/dto/CreateClassRequestDTO.java
@@ -0,0 +1,9 @@
+package com.cscd488seniorproject.syllabussyncproject.dto;
+
+// DTO for handling create class requests
+public class CreateClassRequestDTO {
+    public String classCode;
+    public String quarter;
+    public String year;
+    public String title;
+}
