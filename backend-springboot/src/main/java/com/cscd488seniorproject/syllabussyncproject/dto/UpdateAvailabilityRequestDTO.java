76c674e3281200720491854a06f49edb540f121a
Alex <108487553+Al3xramirez@users.noreply.github.com>
2026-05-11 17:25:58 -0700

    DTOs

---
+++ backend-springboot/src/main/java/com/cscd488seniorproject/syllabussyncproject/dto/UpdateAvailabilityRequestDTO.java
@@ -0,0 +1,11 @@
+package com.cscd488seniorproject.syllabussyncproject.dto;
+
+public class UpdateAvailabilityRequestDTO {
+    public String status;
+
+    public UpdateAvailabilityRequestDTO() {}
+
+    public UpdateAvailabilityRequestDTO(String status) {
+        this.status = status;
+    }
+}
